5f1f032b931b3a4803a8692a6895f655fac35141
breezeqi <qwf88639@gmail.com>
2020-11-09 22:27:48 +0800

    首页完成一半

--- dist/js/shouye.js
+++ dist/js/shouye.js
@@ -1,5 +1,6 @@
 /* 轮播1 */
 var swiper1 = new Swiper('#ba-left-slide', {
+    loop: true,
     autoplay: {
         delay: 3000,
         stopOnLastSlide: false,
@@ -29,6 +30,7 @@ swiper1.el.onmouseout = function() {
 /* 轮播2 */
 
 var swiper2 = new Swiper('#ba-right-slide', {
+    loop: true,
     autoplay: {
         delay: 8000,
         stopOnLastSlide: false,
@@ -50,3 +52,57 @@ swiper2.el.onmouseover = function() {
 swiper2.el.onmouseout = function() {
     swiper2.autoplay.start();
 }
+
+/* 京东秒杀 */
+var oDjs = document.querySelector(".djs");
+var aSpan = oDjs.children;
+
+function getDiffDate() {
+    var date1 = new Date();
+    var date2 = new Date("2020/09/25 18:00:00");
+    var ss = Math.abs((date2 - date1) / 1000);
+    var day = Math.floor(ss / 24 / 3600);
+    var hour = Math.floor(ss / 3600 % 24);
+    var minute = Math.floor(ss / 60 % 60);
+    var second = Math.floor(ss % 60);
+    aSpan[0].innerText = hour;
+    aSpan[1].innerText = minute;
+    aSpan[2].innerText = second;
+}
+setInterval(getDiffDate, 1000);
+
+/* 轮播三长条显示 */
+var swiper = new Swiper('#ba-bottom-center-slide', {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    slidesPerGroup: 4,
+    autoplay: {
+        delay: 8000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true,
+    },
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+/* 轮播四单条显示 */
+var swiper = new Swiper('#ba-bottom-right-slide', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    slidesPerGroup: 1,
+    autoplay: {
+        delay: 2000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true,
+    },
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
